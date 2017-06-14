c8e77b95b34cf9080b144b5a31ffe63cc6f69b8f
Deo <liangzhanzhao@metrodata.cn>
2017-06-14 11:38:18 +0800

    http hook handle connection error

--- airflow/hooks/http_hook.py
+++ airflow/hooks/http_hook.py
@@ -59,7 +59,6 @@ class HttpHook(BaseHook):
         session = self.get_conn(headers)
 
         url = self.base_url + endpoint
-        req = None
         if self.method == 'GET':
             # GET uses params
             req = requests.Request(self.method,
@@ -84,14 +83,17 @@ class HttpHook(BaseHook):
         """
         extra_options = extra_options or {}
 
-        response = session.send(
-            prepped_request,
-            stream=extra_options.get("stream", False),
-            verify=extra_options.get("verify", False),
-            proxies=extra_options.get("proxies", {}),
-            cert=extra_options.get("cert"),
-            timeout=extra_options.get("timeout"),
-            allow_redirects=extra_options.get("allow_redirects", True))
+        try:
+            response = session.send(
+                prepped_request,
+                stream=extra_options.get("stream", False),
+                verify=extra_options.get("verify", False),
+                proxies=extra_options.get("proxies", {}),
+                cert=extra_options.get("cert"),
+                timeout=extra_options.get("timeout"),
+                allow_redirects=extra_options.get("allow_redirects", True))
+        except ConnectionError as err:
+            raise AirflowException('%d:%s' % (500, str(err)))
 
         try:
             response.raise_for_status()
